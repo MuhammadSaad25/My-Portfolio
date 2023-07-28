b0565029b87a1991c2ef2e29313ac8c4d659e8b5
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-28 20:18:08 +0500

    folder structure made

---
+++ components/index.js
@@ -0,0 +1,14 @@
+// home
+import About from "./home/About";
+// import Skills from "./home/Skills";
+//utitls
+import NavBar from "./ui/NavBar";
+// import Footer from "./utils/Footer";
+// Project
+// import Projects from "./Projects/Projects";
+// Chatbot
+import Chatbot from "./chatbot/index";
+// extras
+// import GitHubCalendar from "./home/GitHubCalendar";
+
+export { About, NavBar, Chatbot };
--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,6 @@
 export const projects = [
-	{ name: "react" },
-	{ name: "mongo" },
-	{ name: "react" },
-	{ name: "react" },
-  ];
-  
+  { name: "react" },
+  { name: "mongo" },
+  { name: "react" },
+  { name: "react" },
+];
