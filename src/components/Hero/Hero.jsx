8180a5b737f3f2e36c3905dfbd7582fa4c7a0c2a
Shehzad <muhammadsaad2522@gmail.com>
2023-07-30 16:57:04 +0500

    test push

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({ timeline, ease }) => {
   return (
     <div className="main_hero flex  lg:flex-row flex-col items-center justify-between w-[100%] lg:h-[91.5vh] h-auto">
       <Content timeline={timeline} />
-      <Images timeline={timeline} ease={ease} />
+      {/* <Images timeline={timeline} ease={ease} /> */}
     </div>
   );
 };
