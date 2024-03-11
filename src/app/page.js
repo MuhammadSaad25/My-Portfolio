b63c3ee3d439298a5c9f4b3fd3e877ec096d0a13
Muhammad Saad <muhammadsaad2522@gmail.com>
2024-03-11 16:48:03 +0500

    test i frame

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,7 @@ export default function Home() {
   return (
     <ThemeProvider theme="system" attribute="class">
       <main className={`relative flex  h-screen flex-col items-center ${theme}`}>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/TtIcgRJsyPg?si=Zf3C5tQeJE2OwUdB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
         <video
           autoPlay
